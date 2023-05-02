232d19e686efaab10b345470fb6a766dafb24722
gbouras13 <george.bouras@adelaide.edu.au>
2023-05-02 19:01:27 +0930

    mash bug fix

--- bin/input_commands.py
+++ bin/input_commands.py
@@ -55,8 +55,7 @@ def instantiate_dirs(output_dir, force):
 	# remove outdir on force
 	if force == True:
 		if os.path.isdir(output_dir) == True:
-			#shutil.rmtree(output_dir)
-			print('f')
+			shutil.rmtree(output_dir)
 		else:
 			print("\n--force was specified even though the outdir does not already exist. Continuing \n")
 	else:
